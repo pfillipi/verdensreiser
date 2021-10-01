5c8e8ac00c5e51c7cafbb79a8e728a6bcd069a6a
Fillipi Pereira <pfillipi@gmx.de>
2021-10-01 02:01:46 +0200

    Language

--- src/components/Content/Infos.tsx
+++ src/components/Content/Infos.tsx
@@ -1,5 +1,5 @@
 import { Flex } from "@chakra-ui/react";
 
 export default function Infos() {
-  return <Flex align="center" justify="center"></Flex>;
+  return <Flex align="center" justify="space-between"></Flex>;
 }
