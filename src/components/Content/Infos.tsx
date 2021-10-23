c134697dc4679e3417aa6e747ea974bc60f67b3d
Fillipi Pereira <pfillipi@gmx.de>
2021-10-23 02:57:37 +0200

    Styles

--- src/components/Content/Infos.tsx
+++ src/components/Content/Infos.tsx
@@ -3,8 +3,12 @@ import { Flex, Heading, Text } from "@chakra-ui/react";
 export default function Infos() {
   return (
     <Flex align="center" justify="space-between">
-      <Flex directio="column" justify="center" align={["flex-start"]}>
-        <Heading>femte</Heading>
+      <Flex
+        directio="column"
+        justify="center"
+        align={["flex-start", "flex-start", "center"]}
+      >
+        <Heading fontSize={["2xl", "5xl"]}>femte</Heading>
         <Text>land</Text>
       </Flex>
     </Flex>
