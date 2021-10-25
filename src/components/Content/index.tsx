12bb7aeb74994c61fc98e996ddbaf4b602fde95a
Fillipi Pereira <pfillipi@gmx.de>
2021-10-25 23:51:12 +0200

    Frames

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,5 +1,5 @@
 import { Grid } from "@chakra-ui/react";
 
 export default function Content() {
-  return <Grid templateColumns={["1fr", "1fr fr", "1.2fr 1fr"]}></Grid>;
+  return <Grid templateColumns={["1fr", "1fr 1fr", "1.2fr 1fr"]}></Grid>;
 }
