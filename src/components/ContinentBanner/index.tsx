19ca608c5de0e61936d5939bb67ee28cc2adf9ec
Fillipi Pereira <pfillipi@gmx.de>
2021-11-22 19:11:25 +0100

    :art:

--- src/components/ContinentBanner/index.tsx
+++ src/components/ContinentBanner/index.tsx
@@ -1,5 +1,12 @@
 import { Flex } from "@chakra-ui/react";
 
 export default function ContinentBanner() {
-  return <Flex></Flex>;
+  return (
+    <Flex
+      w="100%"
+      h={["150px", "300px", "500px"]}
+      px={["0", "0", "36"]}
+      pt={["0", "0", "72"]}
+    ></Flex>
+  );
 }
