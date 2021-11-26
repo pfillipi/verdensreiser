b67575b1e6ed4dd5d9499f5aa50086f37646b1fa
Fillipi Pereira <pfillipi@gmx.de>
2021-11-26 23:52:27 +0100

    Gaps

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,5 +1,10 @@
 import { Grid } from "@chakra-ui/react";
 
 export default function Content() {
-  return <Grid templateColumns={["1fr", "1fr 1fr", "1.2fr 1fr"]}></Grid>;
+  return (
+    <Grid
+      templateColumns={["1fr", "1fr 1fr", "1.2fr 1fr"]}
+      gatp={[5, 10, 16, 20]}
+    ></Grid>
+  );
 }
