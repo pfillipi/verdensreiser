e027ae4da421efe5da00bc13521bcb15e86f0249
Fillipi Pereira <pfillipi@gmx.de>
2021-12-07 16:47:54 +0100

    PopoverContent

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -5,7 +5,7 @@ export default function Content() {
   return (
     <Grid
       templateColumns={["1fr", "1fr", "1fr 1fr", "1.2fr 1fr"]}
-      gatp={[5, 10, 16, 20]}
+      gap={[5, 10, 16, 20]}
       my={["8", "20"]}
     >
       <Text
--- src/pages/continent/[slug].tsx
+++ src/pages/continent/[slug].tsx
@@ -1,4 +1,5 @@
 import { Flex } from "@chakra-ui/react";
+import Cities from "../../components/Cities";
 import Content from "../../components/Content";
 import ContinentBanner from "../../components/ContinentBanner";
 import Header from "../../components/Header";
@@ -11,6 +12,7 @@ export default function Continent() {
 
       <Flex direction="column" maxW="1160px" mx="auto" mb="10" px="1rem">
         <Content />
+        <Cities />
       </Flex>
     </Flex>
   );
